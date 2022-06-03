855e30a15dc6deb2958a681ff9be7b75a31d8de9
Yaseen <fantasticyaseenshariff@gmail.com>
2022-06-03 19:40:50 +0530

    update v0.0.5

--- setup.py
+++ setup.py
@@ -9,13 +9,15 @@ readme = (here / "README.md").read_text()
 setup(
   name = 'pygrams',
   packages = ['pygrams'],
-  version = '0.0.3',
+  version = '0.0.4',
   license='MIT',
   description = 'Get python programs handy as a function',
+  long_description=readme,
+  long_description_content_type="text/markdown",
   author = 'Yaseen',
   author_email = 'fantasticyaseenshariff@gmail.com',
   url = 'https://syberstar.com',
-  download_url = 'https://github.com/Yaseen549/pygrams/archive/refs/tags/v0.0.3.tar.gz',
+  download_url = 'https://github.com/Yaseen549/pygrams/archive/refs/tags/v0.0.4.tar.gz',
   keywords = ['PYTHONPROGRAMS', 'PROGRAMS', 'FUNCTIONS', 'PYGRAMS'],
   install_requires=[],
   classifiers=[
